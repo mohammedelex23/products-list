cfa853b4736542e7dfd5ed76a5329daaaefcae7e
Mohammed Ismail Mohammed <62588958+mohammedelex23@users.noreply.github.com>
2023-04-30 23:14:14 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,73 @@
-import './App.css';
-import React, {Component} from 'react';
-import Items from './components/item/items'
-import AddItem from './components/addItem/addItem'
-import Total from './components/total/total'
+import "./App.css";
+import React, { Component } from "react";
+import Items from "./components/item/items";
+import AddItem from "./components/addItem/addItem";
+import Total from "./components/total/total";
 
 class App extends Component {
   state = {
     items: [
-      {id:1, product:'Pen', price:2},
-      {id:2, product:'Book', price:10}
-    ]
-  }
+      { id: 1, product: "Pen", price: 2, qty: 1 },
+      { id: 2, product: "Book", price: 10, qty: 1 },
+    ],
+  };
 
   deleteItem = (id) => {
-    let items = this.state.items
-    let i = items.findIndex(item => item.id === id)
-    items.splice(i, 1)
-    this.setState({items: items})
-  }
+    let items = [...this.state.items];
+    let i = items.findIndex((item) => item.id === id);
+    items.splice(i, 1);
+    this.setState({ items: items });
+  };
 
   addItem = (item) => {
-    this.state.items.length > 0 ? (
-      item.id = this.state.items[this.state.items.length - 1].id + 1 
-    ) : item.id = 1
-    console.log(item.id)
-    let items = this.state.items
-    items.push(item)
-    this.setState({items: items})
-  }
+    this.state.items.length > 0
+      ? (item.id = this.state.items[this.state.items.length - 1].id + 1)
+      : (item.id = 1);
+    console.log(item.id);
+    let items = [...this.state.items];
+    items.push(item);
+    this.setState({ items });
+  };
+
+  updateQty = (id, type) => {
+    let item = this.state.items.find((item) => item.id === id);
+    if (type === "-") {
+      if (item.qty === 1) {
+        return;
+      }
+      let items = this.state.items.map((item) => {
+        if (item.id === id) {
+          item.qty = item.qty - 1;
+        }
+        return item;
+      });
+      this.setState(items);
+    } else {
+      let items = this.state.items.map((item) => {
+        if (item.id === id) {
+          item.qty = item.qty + 1;
+        }
+        return item;
+      });
+      this.setState(items);
+    }
+  };
 
   render() {
     return (
       <div className="container">
         <h1>Product List React App</h1>
         <div className="table">
-          <Items items={this.state.items} del={this.deleteItem}/>
-          <AddItem add={this.addItem}/>
-          <Total items={this.state.items}/>
+          <Items
+            items={this.state.items}
+            updateQty={this.updateQty}
+            del={this.deleteItem}
+          />
+          <AddItem add={this.addItem} />
+          <Total items={this.state.items} />
         </div>
       </div>
-    )
+    );
   }
 }
 

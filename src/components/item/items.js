abb594bd177b81366f2c6c41954390fbb8167640
Mohammed Ismail Mohammed <62588958+mohammedelex23@users.noreply.github.com>
2023-04-30 23:13:27 +0200

    Update items.js

--- src/components/item/items.js
+++ src/components/item/items.js
@@ -1,31 +1,39 @@
-import React from 'react';
+import React from "react";
 
 const Items = (props) => {
-    const {items, del} = props;
-    let length = items.length
-    const ListItem = length ? (
-        items.map(item => {
-            return(
-                <div key={item.id} className="item">
-                    <p>{item.product}</p>
-                    <p>{item.price}</p>
-                    <p className="delete" onClick={() => del(item.id)}>&times;</p>
-                </div>
-            )
-        })
-    ) : (
-        <div className="text">There are no items, Try to add some.</div>
-    )
-    return (
-        <div>
-            <div className="header item">
-                <p>Product</p>
-                <p>Price</p>
-                <p>Edit</p>
-            </div>
-            {ListItem}
+  const { items, del, updateQty } = props;
+  let length = items.length;
+  const ListItem = length ? (
+    items.map((item) => {
+      return (
+        <div key={item.id} className="item">
+          <p>{item.product}</p>
+          <p>{item.price}</p>
+          <p className="qty">
+            <button onClick={() => updateQty(item.id, "-")}>-</button>
+            <span>{item.qty}</span>
+            <button onClick={() => updateQty(item.id, "+")}>+</button>
+          </p>
+          <p className="delete" onClick={() => del(item.id)}>
+            &times;
+          </p>
         </div>
-    )
-}
+      );
+    })
+  ) : (
+    <div className="text">There are no items, Try to add some.</div>
+  );
+  return (
+    <div>
+      <div className="header item">
+        <p>Product</p>
+        <p>Price</p>
+        <p>Quantity</p>
+        <p>Edit</p>
+      </div>
+      {ListItem}
+    </div>
+  );
+};
 
-export default Items
+export default Items;
